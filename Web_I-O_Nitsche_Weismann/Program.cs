50474a45a54b8dbdea95a976286c1bcf14867190
Philipp Weismann <s2110438015@fhooe.at>
2022-01-10 13:26:41 +0100

    Update Main

--- Web_I-O_Nitsche_Weismann/Program.cs
+++ Web_I-O_Nitsche_Weismann/Program.cs
@@ -10,6 +10,22 @@ namespace Web_I_O_Nitsche_Weismann
     {
         static void Main(string[] args)
         {
+            string filePathHealthFitness = @"https://fhwels.s3.eu-central-1.amazonaws.com/PRO1UE_WS21/HealthFitnessApps.CSV";
+            string filePathPhotpgraphy = @"https://fhwels.s3.eu-central-1.amazonaws.com/PRO1UE_WS21/PhotographyApps.CSV";
+            string filePathWeather = @"https://fhwels.s3.eu-central-1.amazonaws.com/PRO1UE_WS21/WeatherApps.CSV";
+
+            LoadDataFromLinks(filePathHealthFitness, filePathPhotpgraphy, filePathWeather);
+        }
+
+        static List<AppData> LoadDataFromLinks(string filePath1, string filePath2, string filePath3)
+        {
+            List<AppData> apps = new List<AppData>();
+
+            apps.AddRange(DataLoader.ReadAppsFromFile(';', filePath1));
+            apps.AddRange(DataLoader.ReadAppsFromFile(';', filePath2));
+            apps.AddRange(DataLoader.ReadAppsFromFile(';', filePath3));
+
+            return apps;
         }
     }
 }
