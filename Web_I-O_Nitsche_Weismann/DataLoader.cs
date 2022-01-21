c30c8d4b1190ec3a0714c4db0b97fd8db16cfcb1
Simon Nitsche <s2110438010@fhooe.at>
2022-01-21 10:18:18 +0100

    Bug Fixes Dataloader (WebClient)

--- Web_I-O_Nitsche_Weismann/DataLoader.cs
+++ Web_I-O_Nitsche_Weismann/DataLoader.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,9 +15,26 @@ namespace Web_I_O_Nitsche_Weismann
         {
             List<AppData> myApps = new List<AppData>();
 
-            StreamReader myStreamReader = new StreamReader(filePath);
+
+            //string bufferFilePath = @"\..\..\buffer.txt";
+
+            //if (File.Exists(bufferFilePath))
+            //{ // Create a file to write to
+            //    File.Delete(bufferFilePath);
+            //}
+            //File.Create(bufferFilePath);
+
+
+            WebClient myWebclient = new WebClient();
+
+            Stream myStream = myWebclient.OpenRead(filePath);
+
+            StreamReader myStreamReader = new StreamReader(myStream);
+
+
             string line;
             int counter = 1;
+
 
             while (myStreamReader.Peek() != -1)
             {
@@ -51,23 +69,28 @@ namespace Web_I_O_Nitsche_Weismann
 
             string[] parts = line.Split(seperator);
 
-            for (int i = 0; i < parts.Length; i++)
-            {
-                parts[i] = parts[i].Replace('.', ',');
-            }
+            //for (int i = 0; i < parts.Length; i++)
+            //{
+            //    parts[i] = parts[i].Replace('.', ',');
+            //}
 
             app.AppName = parts[0];
             app.Category = (myEnums.Category)Enum.Parse(typeof(myEnums.Category), parts[1]);
             app.Rating = int.Parse(parts[2]);
             app.Reviews = int.Parse(parts[3]);
             app.Size = parts[4];
-            app.Installs = int.Parse(parts[5].TrimEnd('+'));
+            app.Installs = parts[5];
             app.PriceType = (myEnums.PriceType)Enum.Parse(typeof(myEnums.PriceType), parts[6]);
-            app.ContentRating = (myEnums.ContentRating)Enum.Parse(typeof(myEnums.ContentRating), parts[7]);
-            app.Genres = (myEnums.Genres)Enum.Parse(typeof(myEnums.Genres), parts[8]);
-            app.LastUpdated = DateTime.Parse(parts[9]);
-            app.CurrentVersion = parts[10];
-            app.AndroidVersion = parts[11];
+            app.Price = double.Parse(parts[7].Replace(',', '.'));
+            //app.ContentRating = (myEnums.ContentRating)Enum.Parse(typeof(myEnums.ContentRating), parts[8]);
+            //app.Genres = (myEnums.Genres)Enum.Parse(typeof(myEnums.Genres), parts[9]);
+            app.LastUpdated = DateTime.Parse(parts[10]);
+            app.CurrentVersion = parts[11];
+            app.AndroidVersion = parts[12];
+
+            //for development:
+            app.ContentRating = myEnums.ContentRating.Everyone;
+            app.Genres = myEnums.Genres.Health_Fitness;
 
             return app;
         }
